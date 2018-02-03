34f977ced77ad969309d67b36ceeedb98e5383c5
Paul Mathis <paul.mathis@gmail.com>
2018-02-03 12:58:47 -0600

    added scss support

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleWare = require('webpack-dev-middleware');
 const open = require('open');
+const shell = require('shelljs');
 
 const app = express();
 const config = require('./webpack.dev.js');
@@ -21,4 +22,7 @@ app.use(require('webpack-hot-middleware')(compiler));
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!\n');
   open('http://localhost:3000');
+
+  // open doesn't support Windows Subsystem for Linux, use this to run wsl-open https://github.com/4U6U57/wsl-open
+  shell.exec('wsl-open http://localhost:3000');
 });
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
+});
 
 module.exports = {
   entry: ['./src/index.jsx'],
@@ -8,7 +14,8 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: 'index.html'
-    })
+    }),
+    extractSass
   ],
   output: {
     filename: '[name].bundle.js',
@@ -17,6 +24,27 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
+      },
       {
         test: /\.(js|jsx|ts|tsx)?$/,
         use: 'ts-loader',
